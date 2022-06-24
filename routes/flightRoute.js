d8bd8d74f114a52164388edb7549c3e086750768
korafdavid <okoroafordavid61@gmail.com>
2022-06-24 10:36:29 +0100

    Task Completed

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router.get('/getflight', controller.single)
+router.get('/getallflight',controller.all)
+router.post('/bookflight',controller.book)
+router.put('updateflight', controller.update)
+router.delete('deleteflight',controller.delete)
+
 
 module.exports = router;
 
